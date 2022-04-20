6095ee5035ee199294aa1bcdd31f8a9cae7541c7
AngelM3ndez <mnceyng@gmail.com>
2022-04-20 04:42:32 +0000

    app.py url

---
+++ app.py
@@ -0,0 +1,12 @@
+import web # se importa la libreria de web.py para hacer sus del framework
+import pyrebase # se importa la libreria de firebase para hacer uso de la fire base creada de google
+import firebase_config as token # se importa la libreria de firebase_comfig para hacer uso de nuestro token de fire base
+import json # se importa la libreria de json para hacer uso y modificación de estos elementos
+
+)
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    web.config.debug = True 
+    app.run()
