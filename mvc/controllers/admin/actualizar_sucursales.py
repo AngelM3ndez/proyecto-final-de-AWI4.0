9a881383b08d132df21b8ca9f68f1e51065ea8f5
AngelM3ndez <mnceyng@gmail.com>
2022-04-26 06:55:33 +0000

    actualizar

--- mvc/controllers/admin/actualizar_sucursales.py
+++ mvc/controllers/admin/actualizar_sucursales.py
@@ -7,30 +7,32 @@ render = web.template.render('mvc/view/admin', base="layout")
 
 class Actualizar_sucursales:
     def GET(self, localId): 
-         try: 
+        try: 
             firebase = pyrebase.initialize_app(token.firebaseConfig) 
             db = firebase.database() 
-            user = db.child("sucursales").child(localId).get()
-            return render.actualizar_sucursales(user)
+            sucursal = db.child("sucursales").child(localId).get()
+            return render.actualizar_sucursales(sucursal)
         except Exception as error: 
             message = "Error en el sistema" 
             print("Error actualizar_sucursales.GET: {}".format(error))  
     
+
     def POST(self, localId):
+        try:
             firebase = pyrebase.initialize_app(token.firebaseConfig) 
             db = firebase.database() 
-            formulario = web.input()
-            print(formulario) 
+            formulario = web.input() 
             name= formulario.name
             temperatura= formulario.temperatura
             humedad = formulario.humedad 
-            email = formulario.email 
-            localid = formulario.localid  
+            email = formulario.email
             data = {
-            "name": name,
-            "temperatura": temperatura,
-            "humedad": humedad,
-            "email": email,
-               }
-            results = db.child("sucursales").child(localid).update(data)
+                "name": name,
+                "temperatura": temperatura,
+                "humedad": humedad,
+                "email": email,
+            }
+            db.child("sucursales").child(localid).update(data)
             return web.seeother("/lista_sucursales")
+        except Exception as error:
+            print("Error actualizar_sucursales update.GET: {}".format(error))
